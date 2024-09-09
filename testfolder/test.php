27519d2b3946907e04897d482174db49622756cf
Кирилл Ляпунов <kirilllapunov@MacBook-Air-Kirill.local>
2024-09-09 17:36:55 +0300

    testfolder/test.php

---
+++ testfolder/test.php
@@ -0,0 +1,26 @@
+div.gr-spt-title, h2.gr-spt-title, h3.gr-spt-title, h4.gr-spt-title, h5.gr-spt-title, h6.gr-spt-title {
+    font-weight: bold;
+}
+
+
+есть модуль для cs-cart "GR: SEO для товарных вкладок"
+Как мне его правильно перенести с одного домена на другой? 
+
+
+
+
+
+  
+
+
+@media (min-width: 768px) {
+    .ty-variations-list.ty-table td[class*="product-elem"] {
+        border: 0;
+        padding-left: 15px;
+        padding-right: 0;
+        border-bottom: 1px solid #eee;
+        justify-content: center;
+    }
+}
+
+
